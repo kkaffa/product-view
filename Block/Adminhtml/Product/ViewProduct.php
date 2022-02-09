bb2423968a46638f5db15c48d1801663ab91b081
Kev <k.kaffa@xsarus.nl>
2022-02-09 13:59:50 +0100

    Remove imports that aren't used anymore

--- Block/Adminhtml/Product/ViewProduct.php
+++ Block/Adminhtml/Product/ViewProduct.php
@@ -3,42 +3,26 @@
 namespace Kkaffa\ProductView\Block\Adminhtml\Product;
 
 use Magento\Catalog\Block\Adminhtml\Product\Edit\Button\Generic;
-use Magento\Catalog\Model\Product;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Registry;
-use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\UiComponent\Context;
-use Magento\Store\Model\App\Emulation;
-use function xsarus\ProductView\Block\Adminhtml\Product\__;
 
 class ViewProduct extends Generic
 {
     protected RequestInterface $_request;
-    protected Product $_product;
-    protected Emulation $_emulation;
-    protected UrlInterface $_url;
 
     /**
      * @param RequestInterface $_request
-     * @param Product $_product
-     * @param Emulation $_emulation
-     * @param UrlInterface $_url
      * @param Context $context
      * @param Registry $registry
      */
     public function __construct(
         RequestInterface $_request,
-        Product          $_product,
-        Emulation        $_emulation,
-        UrlInterface     $_url,
         Context          $context,
         Registry         $registry
     )
     {
         $this->_request = $_request;
-        $this->_product = $_product;
-        $this->_emulation = $_emulation;
-        $this->_url = $_url;
         parent::__construct($context, $registry);
     }
 
@@ -48,7 +32,6 @@ class ViewProduct extends Generic
      */
     public function getButtonData(): array
     {
-        $this->getProductUrl();
         if ($this->checkRequestAction()) {
             return [
                 'label' => __('View Product'),
